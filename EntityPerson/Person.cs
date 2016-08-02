8b2c9bb4d61eb66ec49cc4e247d8350e847e2b3d
Sheard Z <zsheng12@gmail.com>
2016-08-01 21:31:08 -0400

    Entity Layer

---
+++ EntityPerson/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityPerson
+{
+
+    public class PersonLong
+    {
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string City { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public int Age { get; set; }
+    }
+
+    public class PersonShort
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
